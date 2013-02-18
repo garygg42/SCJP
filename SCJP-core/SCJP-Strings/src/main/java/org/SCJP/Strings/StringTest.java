3a120efa301d628ad51bd3e609c1fe7456806a06
gary <garygg42@gmail.com>
2013-02-18 17:20:53 +0200

    new SCJP

---
+++ SCJP-core/SCJP-Strings/src/main/java/org/SCJP/Strings/StringTest.java
@@ -0,0 +1,14 @@
+package org.SCJP.Strings;
+
+public class StringTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("0123456789");
+        System.out.println(sb.delete(4, 6));
+
+    }
+
+}
